be6735a931643b64227fb9660e9e1a76d7c3e559
Your Name <your.email@example.com>
2025-02-03 08:24:48 +0530

    p

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const database=require("./config/database")
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -24,14 +24,25 @@ app.use("/api/orders", orderRoutes)
 app.use("/api/auth", userRoutes)
 app.use("/api/admin", adminRoutes)
 
-async function main() {
-  await mongoose.connect(process.env.DB_URL);
-  app.use("/", (req, res) => {
-    res.send("Book Store Server is running!");
-  });
-}
+// async function main() {
+//   await mongoose.connect(process.env.DB_URL);
+//   app.use("/", (req, res) => {
+//     res.send("Book Store Server is running!");
+//   });
+// }
 
-main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+//main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+
+//database connect
+database.connect();
+//default route
+app.get("/", (req, res) => {
+  console.log(req)
+  return res.json({
+  success:true,
+  message:'Your server is up and running....'
+});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
